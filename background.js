2a6e5b20151ae6df5ce8b83b06fb0712350782ee
Anshul Saha <anshulsaha15@gmail.com>
2021-02-12 18:53:24 +0530

    ui + repo selector + create repo webhook

--- background.js
+++ background.js
@@ -12,9 +12,12 @@ chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
         })     
     } 
     if (message.text == "logout") {
-        chrome.storage.local.set({accesstoken: null, username: null}, function() {
-            chrome.runtime.reload()
-        });
+        const logoutBoolean = confirm("Are you sure you want to logout?")
+        if(logoutBoolean) {
+            chrome.storage.local.set({accesstoken: null, username: null}, function() {
+                chrome.runtime.reload()
+            });
+        }
     } 
 });
 
@@ -34,6 +37,70 @@ chrome.runtime.onMessage.addListener(
         return true;
     }
   );
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.text == "getRepos") {
+            chrome.storage.local.get(['accesstoken'], function(result) {
+                if(result.accesstoken != null) {
+                    chrome.storage.local.get(['username'], function(result) {
+                        const requestOptions = {
+                            method: 'GET',
+                            redirect: 'follow'
+                          };
+                        
+                        fetch(`https://api.github.com/users/${result.username}/repos`, requestOptions)
+                            .then(async function(results) {
+                                const parsed = await results.json()
+                                let options = []
+                                for(let i = 0; i < parsed.length; i++) {
+                                    options.push(parsed[i].name);
+                                }
+                                sendResponse({message: options})
+                            })
+                            .catch(error => sendResponse({message: "error"}));
+                    })
+                } else {
+                   sendResponse({message: "error"}) 
+                }
+            }) 
+        }
+        return true;
+    }
+  );
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.text == "addRepoWebhook") {
+            const repoName = request.repoName
+            chrome.storage.local.get(['accesstoken'], function(result) {
+                if(result.accesstoken != null) {
+                    const accesstoken = result.accesstoken
+                    chrome.storage.local.get(['username'], function(result) {
+                        const username = result.username
+                        const requestOptions = {
+                            method: 'GET',
+                            redirect: 'follow'
+                            };
+                        
+                        fetch(`https://starhub.ml/webhook?repository=${repoName}&user=${username}&token=${accesstoken}`, requestOptions)
+                            .then(async function(result) {
+                                if(result == "repo already exists") {
+                                    sendResponse({message: "repo is already being tracked", type: "negative"})
+                                } else {
+                                    sendResponse({message: "repo added", type: "positive"})
+                                }
+                            })
+                            .catch(error => sendResponse({message: "error", type: "negative"}));
+                    })
+                } else {
+                    sendResponse({message: "error", type: "negative"}) 
+                }
+            }) 
+        }
+        return true;
+    }
+);
 
 async function auth() {
     chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
--- popup.js
+++ popup.js
@@ -5,6 +5,41 @@ document.addEventListener("click", function (e) {
     if(e.target.id == "logoutButton") {
         chrome.runtime.sendMessage({text: "logout"})
     }
+    if(e.target.id == "aboutButton") {
+        window.open("https://starhub.ml")
+    }
+    if(e.target.id == "addRepoButton") {
+        document.getElementById("repos").style.display = "none";
+        document.getElementById("message").style.display = "block";
+        chrome.runtime.sendMessage({text: "getRepos"}, function(response) {
+            if(response.message == "error") {
+                document.getElementById("message").innerHTML = "an error occurred";
+            } else {
+                const options = response.message
+                const repoSelector = document.getElementById("repoSelector")
+                for(let i=0; i < options.length;i++) {
+                    const option = '<option value="' + options[i] + '">' + options[i] + '</option>'
+                    repoSelector.innerHTML = repoSelector.innerHTML + option
+                }
+                document.getElementById("message").style.display = "none";
+                document.getElementById("addRepo").style.display = "block";
+            }
+        })
+    }
+    if(e.target.id == "addRepoAfterSelectButton") {
+        const e = document.getElementById("repoSelector");
+        const repoName = e.options[e.selectedIndex].value;
+        chrome.runtime.sendMessage({text: "addRepoWebhook", repoName: repoName}, function(response) {
+            document.getElementById("addRepo").style.display = "none";
+            document.getElementById("message").style.display = "block";
+            if(response.type == "negative") {                
+                document.getElementById("message").innerHTML = response.message;
+            } else {
+                // update cached list of tracking repos
+                location.reload(); 
+            }
+        })
+    }
 });
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -12,9 +47,15 @@ document.addEventListener('DOMContentLoaded', function () {
         if(response.message == false) {
             document.getElementById("authButton").style.display = "block";
         } else {
-            document.getElementById("username").innerHTML = 'username: ' + response.message;
+            // get list of tracking repos if cached version not found, if found use that
+            // inflate the table with the list obtained above
+            document.getElementById("username").innerHTML = response.message;
+            document.getElementById("pfp").src = "https://github.com/"+response.message+".png";
             document.getElementById("username").style.display = "block";
+            document.getElementById("pfp").style.display = "block";
+            document.getElementById("repos").style.display = "block";
             document.getElementById("logoutButton").style.display = "block";
+            document.getElementById("aboutButton").style.display = "block";
         }
     })
 });
